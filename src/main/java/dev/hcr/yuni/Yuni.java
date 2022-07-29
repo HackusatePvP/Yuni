a63edebc310aa36ffaaa680f957b25819710bf45
Hackusate_PvP <Supermftw@gmail.com>
2022-07-29 02:56:01 -0500

    Initial commit

---
+++ src/main/java/dev/hcr/yuni/Yuni.java
@@ -0,0 +1,17 @@
+package dev.hcr.yuni;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Yuni extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
